ed71bd0f9902009d2a465d116e9e9a1a3f0a445e
cirno-nine <ghost2583855829@outlook.com>
2024-04-20 19:56:09 +0800

    lab1 commit

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *f = head;
+    node *n = head;
+    while(f != NULL) {
+      f = f->next;
+      if(f == NULL) break;
+      f = f->next;
+      n = n->next;
+      if(f == n) return 1;
+    }
     return 0;
 }
